e970de4747e4e23208c80275edf5080f5ff464ad
Mary <pereverzeva.mary@gmail.com>
2022-05-27 19:19:32 +0300

    lesson3 files added

--- mainapp/views.py
+++ mainapp/views.py
@@ -2,28 +2,35 @@ import json
 
 from django.shortcuts import render
 
+from geekshop.settings import BASE_DIR
+from mainapp.models import Product, Category
+
 
 def index(request):
-    return render(request, 'mainapp/index.html')
+    context = {
+        'products': Product.objects.all()[:4]
+    }
+
+    return render(request, 'mainapp/index.html', context)
 
 
 def products(request):
-    links_menu = [
-        {'href': 'products_all', 'title': 'все'},
-        {'href': 'products_home', 'title': 'дом'},
-        {'href': 'products_office', 'title': 'офис'},
-        {'href': 'products_modern', 'title': 'модерн'},
-        {'href': 'products_classic', 'title': 'классика'},
-    ]
-
     context = {
-        'links_menu': links_menu
+        'links_menu': Category.objects.all()
+    }
+    return render(request, 'mainapp/products.html', context)
+
+
+def products_list(request, pk):
+    print(pk)
+    context = {
+        'links_menu': Category.objects.all()
     }
     return render(request, 'mainapp/products.html', context)
 
 
 def contact(request):
-    with open('contacts.json') as contacts_json:
+    with open(f'{BASE_DIR}/contacts.json', encoding="utf-8") as contacts_json:
         contacts = json.load(contacts_json)
 
         context = {
