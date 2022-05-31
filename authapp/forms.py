24eeb30f483d652dd86c09534379e294669119fc
Mary <pereverzeva.mary@gmail.com>
2022-05-31 17:39:07 +0300

    lesson4 files added

---
+++ authapp/forms.py
@@ -0,0 +1,64 @@
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, UserChangeForm
+from django import forms
+
+from authapp.models import ShopUser
+
+
+class ShopUserLoginForm(AuthenticationForm):
+
+    class Meta:
+        model = ShopUser
+        fields = ('username', 'password',)
+
+    def __int__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+
+
+class ShopUserRegisterForm(UserCreationForm):
+
+    class Meta:
+        model = ShopUser
+        fields = ('username', 'first_name', 'last_name', 'email', 'age', 'avatar', 'password1', 'password2',)
+
+    def __int__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+
+    def clean_age(self):
+        current_age = self.cleaned_data['age']
+        if current_age < 18:
+            raise forms.ValidationError('Вы слишком молоды')
+
+        return current_age
+
+
+class ShopUserEditForm(UserCreationForm):
+
+    class Meta:
+        model = ShopUser
+        fields = ('username', 'first_name', 'last_name', 'email', 'age', 'avatar', 'password1', 'password2',)
+
+    def __int__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+            field.help_text = ''
+            if field_name == 'password':
+                field.widget = forms.HiddenInput()
+
+    def clean_age(self):
+        current_age = self.cleaned_data['age']
+        if current_age < 18:
+            raise forms.ValidationError('Вы слишком молоды')
+
+        return current_age
+
+    def clean_email(self):
+        current_email = self.cleaned_data['email']
+        if ShopUser.objects.filter(email=current_email).exists():
+            raise forms.ValidationError('Пользователь с таким email уже зарегистрирован')
+
+        return current_email
